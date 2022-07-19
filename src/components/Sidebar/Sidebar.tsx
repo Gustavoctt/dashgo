75b38628804a8fc84c94d4bc68e83b3efba830be
Gustavoctt <gustavocarrertartare@gmail.com>
2022-07-19 18:24:09 -0300

    Create Dashboards

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } fr
 export function Sidebar(){
   return(
     <Box>
-      <Stack spacing="12" align="flex-start">
+      <Stack spacing="12" w={64} mr="6" align="flex-start">
         <Box>
           <Text fontWeight="bold" color="gray.400" fontSize="small" >GERAL</Text>
           <Stack spacing="4" mt="8" align="stretch">
