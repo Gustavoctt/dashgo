7d214d5b51ee2386ed70948eb8c387b0c26cb08e
Gustavoctt <gustavocarrertartare@gmail.com>
2022-07-21 18:51:41 -0300

    Create Sidebar Context

--- src/components/Header/Porfile.tsx
+++ src/components/Header/Porfile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfilePorps{
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfilePorps){
   return(
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Gustavo Tartare</Text>
-        <Text color="gray.300" fontSize="small" >
-          gustavocarrertartare@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gustavo Tartare</Text>
+          <Text color="gray.300" fontSize="small" >
+            gustavocarrertartare@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Gustavo Tartare" src="https://github.com/Gustavoctt.png"/>
     </Flex>
